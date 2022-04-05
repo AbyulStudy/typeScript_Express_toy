2eb691e242c108cc90cbdb363182efd1ad3f787f
devbyul <kinsng1599@gmail.com>
2022-04-05 18:30:03 +0900

    feat: ProcessEnv add to JWT_SECRET_KEY: string

--- node.d.ts
+++ node.d.ts
@@ -5,12 +5,13 @@ declare namespace NodeJS {
 
   interface ProcessEnv {
     NODE_ENV: string;
+    PORT: string;
 
     DB_HOST: string;
     DB_PORT: string;
     DB_USER: string;
     DB_PASSWORD: string;
 
-    PORT: string;
+    JWT_SECRET_KEY: string;
   }
 }

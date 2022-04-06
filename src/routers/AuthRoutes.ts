657d899141c1e9c5d97f7e9b7fa33063ea296614
devbyul <kinsng1599@gmail.com>
2022-04-06 10:50:52 +0900

    feat: [GET] profile with AuthMiddleware.auth

--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,5 +1,7 @@
 import BaseRouter from './BaseRouter';
 import validate from '../middlewares/AuthValidator';
+import { auth } from '../middlewares/AuthMiddleware';
+
 // Controllers
 import AuthController from '../controllers/AuthController';
 
@@ -7,6 +9,7 @@ class AuthRoutes extends BaseRouter {
   public routes(): void {
     this.router.post('/register', validate, AuthController.register);
     this.router.post('/login', AuthController.login);
+    this.router.get('/profile', auth, AuthController.profile);
   }
 }
 
